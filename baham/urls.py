079889238fc1f7df0dff4bc781ee39877a0ae15b
rameentauseef <64321541+rameentauseef@users.noreply.github.com>
2023-05-18 09:25:26 -0700

    Update urls.py

--- baham/urls.py
+++ baham/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('baham/vehicles/create', views.create_vehicle, name='createvehicle'),
     path('baham/vehicles/save/', views.save_vehicle, name='savevehicle'),
     path('baham/aboutus', views.view_aboutus, name='aboutus'),
+    path('veh_delete/<int:pk>/void/', views.veh_delete, name='veh_delete'),
+    path('veh_nodelete/<int:pk>/unvoid/', views.veh_nodelete, name='veh_nodelete'),
 ]
